b58ce362b2eec91f0c7850fab9dc1b3dfda65b75
mudit2812 <muditpandey2812@gmail.com>
2020-10-26 03:07:43 -0700

    Working on complex logic

--- server/controllers/searchController.js
+++ server/controllers/searchController.js
@@ -1,3 +1,5 @@
+const Doctor = require("../models/doctor");
+
 const symptomToDisease = (req, res) => {
   console.log("lol");
 };
@@ -10,6 +12,39 @@ const findDoctor = (req, res) => {
   symptomToDisease(req, res);
 
   diseaseToSpecialization(req, res);
+
+  const spec = ["spec1", "spec2", "spec3"];
+
+  const sortedDocs = [];
+
+  for (var j = 0; j < 3; j++) {
+    Doctor.find({ specialization: spec[i] })
+      .lean()
+      .then((result) => {
+        for (var i = 0; i < result.length - 1; i++) {
+          var minInd = i;
+          for (var k = i + 1; k < result.length; k++) {
+            if (
+              result[k].get("years_of_experience") >
+              result[minInd].get("years_of_experience")
+            )
+              minInd = k;
+
+            var tmp = result[k];
+            result[k] = result[i];
+            result[i] = tmp;
+          }
+        }
+
+        for (var i = 0; i < result.length; i++) sortedDocs.push(result[i]);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.send(err);
+      });
+  }
+
+  res.send(sortedDocs);
 
   console.log(res.locals.user);
   res.send(res.locals.user);
