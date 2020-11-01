91a16d3da0cf8e962a657c756662462f123bfe44
mudit2812 <muditpandey2812@gmail.com>
2020-11-01 15:49:08 -0800

    Testing a function without mocking to see if travisCI populates database correctly

--- server/tests/user.test.js
+++ server/tests/user.test.js
@@ -9,6 +9,7 @@ const Patient = require("../models/patient");
 
 const patientRouter = require("../routes/patientRoutes");
 const { initMongo, closeMongo } = require("../config/db");
+const { forEach } = require("async");
 
 const app = express();
 app.use(express.json());
@@ -60,4 +61,50 @@ test("Expect to get 1 patient when making request to get a list of all patients"
   expect(res.body[0].last_name).toBe("Joe");
 
   expect(getUserMock.mock.calls.length).toBe(1);
+});
+
+test("Expect to get no doctors when making request to get a list of all doctors", async () => {
+  getUserMock = jest.fn(async (req, res, model) =>
+    model === Doctor
+      ? res.status(200).json([])
+      : model === Patient
+      ? res.status(200).json([
+          {
+            appointments: [],
+            userkey: "Patient",
+            _id: "5f9ce563b761fe125cece98e",
+            first_name: "Mary",
+            last_name: "Joe",
+            email: "maryjoe@gmail.com",
+            password:
+              "$2a$10$MBWwvXEaojmr76GOwhcq1Ob5jd9WzdURxBv/lEdQ6FYuV5hI61BR2",
+            age: 30,
+            gender: "Female",
+            height: 170,
+            weight: 60,
+            createdAt: "2020-10-31T04:17:39.398Z",
+            updatedAt: "2020-10-31T04:17:39.398Z",
+            __v: 0,
+          },
+        ])
+      : res.status(400)
+  );
+
+  user.getUser = getUserMock;
+
+  const res = await supertest(app).get("/doctor/");
+  expect(res.body.length).toBe(0);
+  expect(getUserMock.mock.calls.length).toBe(1);
+});
+
+test("Expect to get all patients when making request to get a list of all patients (no mocking)", async () => {
+  const res = await supertest(app).get("/patient/");
+  expect(res.body.length).toBe(8);
+  res.body.forEach((item) => {
+    expect(item.userkey).toBe("Patient");
+    expect(item.age).toBeGreaterThanOrEqual(0);
+    expect(item.first_name).toBeInstanceOf(String);
+    expect(item.last_name).toBeInstanceOf(String);
+    expect(item.gender).toBeInstanceOf(String);
+  });
 });
