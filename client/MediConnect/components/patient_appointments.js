557fa7706ea22c9417a454f36a4bbc039efab913
srahul14 <shandilyaholy@gmail.com>
2020-11-26 18:16:39 -0800

    slot booking

--- client/MediConnect/components/patient_appointments.js
+++ client/MediConnect/components/patient_appointments.js
@@ -8,26 +8,6 @@ import { ScrollView } from 'react-native-gesture-handler';
 
 class PatientAppointments extends Component {
 
-	state = {
-		serverData: [],
-	}
-
-	componentDidMount = () => {
-		const uid = global.userID
-		axios
-			.get("http://54.183.200.234:5000/patient/appointment/" + uid, {
-			//.get("http://10.0.2.2:5000/patient/appointment" + uid, {
-			})
-			.then((res) => {
-				console.log(res.data);
-				this.setState({
-					serverData: res.data,
-				});
-			})
-			.catch((err) => {
-				console.log(err.response.data);
-			});
-	};
 	/*
 	componentDidMount = () => {
 		axios
@@ -57,24 +37,19 @@ class PatientAppointments extends Component {
 			// 	colors={['#ffffff', '#ffffff', 'rgba(2, 217, 188, 0.2)']}
 			// 	style={styles.LinearGradient}
 			// >
-				<ScrollView>
-					<View style={styles.container}>
-						<Text style={styles.header}>Appointments</Text>
-
-						<View>
-							{this.state.serverData.map(serverData =><View style={styles.doctor} key={serverData.password}>
-						
-							<Text style={styles.doctorinfo}>{'Doctor Name: ' + serverData.first_name +' ' + serverData.last_name}</Text>
-							<Text style={styles.doctorinfo}>{'Email: ' + serverData.email}</Text>
-							<Text style={styles.doctorinfo}>{'Speciality: ' + serverData.speciality}</Text>
-							<Text style={styles.doctorinfo}>{'Appointment Date: ' + serverData.date}</Text>
-							<Text style={styles.doctorinfo}>{'Booked Slot: ' + serverData.slot}</Text>
-						
-							</View>)}	
-						</View>
-
+				<View style={styles.container}>
+					<View>
+						<Text style={styles.headerText}>Appointments</Text>
 					</View>
-				</ScrollView>
+
+					<View>
+						<TouchableOpacity style={styles.option}>
+							<Text style={styles.optionText}
+							onPress={() => this.props.navigation.navigate('CreateAppointment')}>Create an appointment</Text>
+						</TouchableOpacity>
+					</View>
+
+				</View>
 			/* </LinearGradient> */
 		);
 	}
@@ -87,17 +62,7 @@ const styles = StyleSheet.create({
 	},
 
 	container: {
-		flex: 1,
-		backgroundColor: 'white',
-		fontFamily: 'Iowan Old Style',
-		width: '100%',
-		height: '100%',
 		padding: 30,
-	},
-
-	header: {
-		color: '#02f0c8',
-		fontSize: 20,
 	},
 
 	icon: {
@@ -142,49 +107,7 @@ const styles = StyleSheet.create({
 		elevation: 8,
 		width: 270,
 		padding: 5,
-	},
-
-	text: {
-		color: '#5c5c5c',
-		fontSize: 15,
-	},
-
-	button: {
-		backgroundColor: 'white',
-		padding: 10,
-		margin: 15,
-		height: 40,
-		alignItems: 'center',
-		justifyContent: 'center',
-		shadowColor: 'black',
-		borderRadius: 7,
-	},
-
-	buttonText: {
-		fontFamily: 'Iowan Old Style',
-		fontSize: 15,
-		color: '#02d9b5',
-	},
-
-	doctor: {
-		backgroundColor: '#d9d9d9',
-		height: 120,
-		width: 270,
-		margin: 15,
-		borderRadius: 5,
-		shadowColor: 'black',
-		shadowOpacity: 1,
-		shadowRadius: 4.65,
-		elevation: 8,
-		padding: 15,
-		alignSelf: 'center',
-	},
-
-	doctorinfo: {
-		fontFamily: 'Iowan Old Style',
-		color: 'black',
-		fontSize: 11,
-	},
+	}
 });
 
 export default PatientAppointments;
